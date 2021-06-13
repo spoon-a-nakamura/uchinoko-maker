90ad7738cbd136294abde682284b02e9ff79024a
a-nakamura <a-nakamura@studio-spoon.co.jp>
2021-06-13 15:54:35 +0900

    Mainの作成

---
+++ src/components/pages/Home/Main.tsx
@@ -0,0 +1,196 @@
+import { VFC } from 'react';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { colors } from '../../../styles/utils';
+
+const types = [
+  {
+    id: 1,
+    label: 'からだ',
+    value: 'body',
+    length: 8,
+  },
+  {
+    id: 2,
+    label: 'いろ',
+    value: 'color',
+    length: 8,
+  },
+  {
+    id: 3,
+    label: 'め',
+    value: 'eye',
+    length: 8,
+  },
+  {
+    id: 4,
+    label: 'くち',
+    value: 'mouse',
+    length: 6,
+  },
+  {
+    id: 5,
+    label: 'みみ',
+    value: 'ear',
+    length: 6,
+  },
+  {
+    id: 6,
+    label: 'ひげ',
+    value: 'beard',
+    length: 5,
+  },
+];
+
+const Main: VFC = () => {
+  // const [mainImage, setMainImage] = useState(1);
+  const [shapeId, setShapeId] = useState(Array(types.length).fill(1));
+  const [activeTypesId, setActiveTypesId] = useState(1);
+
+  const setShapesState = (typesIndex: number, selectIndex: number) => {
+    setShapeId((currentStates) => {
+      const newShapesState = currentStates.map((currentState, stateIndex) => {
+        console.log(`stateIndex${stateIndex}`);
+        return typesIndex - 1 === stateIndex ? selectIndex : currentState;
+      });
+      return newShapesState;
+    });
+  };
+  return (
+    <>
+      <Root>
+        <MainImageWrapper>
+          <MainImage
+            src={`/assets/images/cats/body/${shapeId[0]}@2x.png`}
+            alt=""
+          />
+        </MainImageWrapper>
+        <SelectShapesWrapper>
+          {types.map((type) => {
+            return (
+              activeTypesId === type.id && (
+                <SelectShapes key={type.id}>
+                  {[...Array(type.length)].map((_, index) => {
+                    return (
+                      <SelectShape
+                        key={index}
+                        shapeId={shapeId[activeTypesId - 1]}
+                        selectedId={index + 1}
+                      >
+                        <Thumbnail
+                          src={`/assets/images/cats/${type.value}/${
+                            index + 1
+                          }@2x.png`}
+                          alt=""
+                          onClick={() =>
+                            setShapesState(activeTypesId, index + 1)
+                          }
+                        />
+                      </SelectShape>
+                    );
+                  })}
+                </SelectShapes>
+              )
+            );
+          })}
+        </SelectShapesWrapper>
+        <SelectTypesWrapper>
+          <SelectTypes>
+            {types.map((type) => {
+              return (
+                <SelectType
+                  key={type.id}
+                  typeId={activeTypesId}
+                  selectedId={type.id}
+                >
+                  <SelectTypeThumbnail
+                    src={`/assets/images/cats/types/${type.id}@2x.png`}
+                    alt=""
+                    onClick={() => setActiveTypesId(type.id)}
+                  />
+                  <SelectTypeLabel>{type.label}</SelectTypeLabel>
+                </SelectType>
+              );
+            })}
+          </SelectTypes>
+        </SelectTypesWrapper>
+      </Root>
+    </>
+  );
+};
+export default Main;
+
+const Root = styled.div`
+  width: 100%;
+  background: ${colors.white};
+`;
+const MainImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const MainImage = styled.img`
+  width: 80%;
+`;
+const SelectShapesWrapper = styled.div`
+  background: ${colors.powderBlue};
+`;
+const SelectShapes = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: center;
+  padding: 10px;
+`;
+type SelectShapeProps = {
+  shapeId: number;
+  selectedId: number;
+};
+const SelectShape = styled.li<SelectShapeProps>`
+  width: calc(25% - 10px);
+  background: ${colors.white};
+  border-radius: 16px;
+  margin: 5px;
+  border: 3px solid;
+  border-color: ${({ shapeId, selectedId }) =>
+    shapeId === selectedId ? colors.aquaBlue : colors.white};
+`;
+const Thumbnail = styled.img`
+  cursor: pointer;
+  padding: 5px;
+`;
+
+const SelectTypesWrapper = styled.div`
+  background: ${colors.milkyPink};
+  overflow-x: auto;
+`;
+const SelectTypes = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 10px;
+  width: fit-content;
+`;
+type SelectTypeProps = {
+  typeId: number;
+  selectedId: number;
+};
+const SelectType = styled.li<SelectTypeProps>`
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+  background: ${colors.white};
+  border-radius: 100px;
+  margin: 5px;
+  border: 2px solid;
+  border-color: ${({ typeId, selectedId }) =>
+    typeId === selectedId ? colors.shellPink : colors.white};
+  margin-bottom: 20px;
+`;
+const SelectTypeThumbnail = styled.img``;
+
+const SelectTypeLabel = styled.p`
+  text-align: center;
+  font-size: 12px;
+  margin-top: 5px;
+`;
